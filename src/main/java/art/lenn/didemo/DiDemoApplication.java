e7321261da76b6780cb506cf6ef6a7e3dce85921
Lennart Cockx <Lennart.cockx@faros.be>
2018-11-28 15:54:52 +0100

    Demonstrated the ways to capture beans from the spring application context Added comments to clarify code Tested the beans by calling their method

--- src/main/java/art/lenn/didemo/DiDemoApplication.java
+++ src/main/java/art/lenn/didemo/DiDemoApplication.java
@@ -1,5 +1,6 @@
 package art.lenn.didemo;
 
+import art.lenn.didemo.controllers.InjectionController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,5 +10,16 @@ public class DiDemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+
+		// Will capture the bean named: injectionController
+		// Works with either the name (needs cast) or via the class Type
+		InjectionController contrViaBeanName = (InjectionController) context.getBean("injectionController");
+		InjectionController contrViaClassName = context.getBean(InjectionController.class);
+
+		if (contrViaBeanName==contrViaClassName){ System.out.println("Both instances are the same"); }
+		else { System.out.println("These instances are different"); }
+
+		contrViaBeanName.inject();
+		contrViaClassName.inject();
 	}
 }
--- src/main/java/art/lenn/didemo/controllers/InjectionController.java
+++ src/main/java/art/lenn/didemo/controllers/InjectionController.java
@@ -2,6 +2,8 @@ package art.lenn.didemo.controllers;
 
 import org.springframework.stereotype.Controller;
 
+// @Controller annotation will make a bean from this class and instanciate it when the application context is created
+// This bean will be of Type InjectionController and will have the name "injectionController" (Lowercase i)
 @Controller
 public class InjectionController {
 
