aebdb6a08bbe5da7ac7bda2268ff967d5dd8ab88
Lennart Cockx <Lennart.cockx@faros.be>
2018-11-28 14:24:49 +0100

    Created injection controller

--- src/main/java/art/lenn/didemo/DiDemoApplication.java
+++ src/main/java/art/lenn/didemo/DiDemoApplication.java
@@ -2,11 +2,12 @@ package art.lenn.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
 	}
 }
---
+++ src/main/java/art/lenn/didemo/controllers/InjectionController.java
@@ -0,0 +1,11 @@
+package art.lenn.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class InjectionController {
+
+    public void inject(){
+        System.out.println("Running");
+    }
+}
