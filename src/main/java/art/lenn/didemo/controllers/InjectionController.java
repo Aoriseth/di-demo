56ebe4941c69fb8e146fdb50f077bd5f7c65986b
Lennart Cockx <Lennart.Cockx@faros.be>
2018-12-10 11:08:23 +0100

    Added a @Primary service option that will take precedence in a multiple-option scenario Modified injectionController to search for services and use them. (This should call the primary service)

--- src/main/java/art/lenn/didemo/controllers/InjectionController.java
+++ src/main/java/art/lenn/didemo/controllers/InjectionController.java
@@ -1,5 +1,6 @@
 package art.lenn.didemo.controllers;
 
+import art.lenn.didemo.services.InjectionService;
 import org.springframework.stereotype.Controller;
 
 // @Controller annotation will make a bean from this class and instanciate it when the application context is created
@@ -7,7 +8,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class InjectionController {
 
+    private InjectionService injectionService;
+
+    public InjectionController(InjectionService injectionService) {
+        this.injectionService = injectionService;
+    }
+
     public void inject(){
         System.out.println("Running");
+        System.out.println(injectionService.messageCall());
     }
 }
---
+++ src/main/java/art/lenn/didemo/services/PrimaryInjectionService.java
@@ -0,0 +1,15 @@
+package art.lenn.didemo.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+// The primary service will always be used when multiple options for bean injection are possible.
+// Primary will take precedence over other ways that spring figures out multiple options
+@Service
+@Primary
+public class PrimaryInjectionService implements InjectionService {
+    @Override
+    public String messageCall() {
+        return "Injection via the primary annotation.";
+    }
+}
