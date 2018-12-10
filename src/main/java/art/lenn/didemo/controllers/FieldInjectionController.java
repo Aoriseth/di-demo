5337320a9f10b1f0fe3ca34ea70b6e7b8ab44ea6
Lennart Cockx <Lennart.Cockx@faros.be>
2018-12-10 10:19:44 +0100

    Test without Spring injection. Running the application should fail Tests should still succeed

--- src/main/java/art/lenn/didemo/controllers/FieldInjectionController.java
+++ src/main/java/art/lenn/didemo/controllers/FieldInjectionController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class FieldInjectionController {
 
-    @Autowired
+
     public InjectionServiceImpl injectionService;
 
     public String testService(){
