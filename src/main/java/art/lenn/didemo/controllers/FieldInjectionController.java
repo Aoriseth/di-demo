a42b83369f7750c0b26940279ba3f9380e4e4f3b
Lennart Cockx <Lennart.Cockx@faros.be>
2018-12-10 11:12:24 +0100

    Modified FieldInjectionController to use Qualifier because of changes with Primary

--- src/main/java/art/lenn/didemo/controllers/FieldInjectionController.java
+++ src/main/java/art/lenn/didemo/controllers/FieldInjectionController.java
@@ -2,6 +2,7 @@ package art.lenn.didemo.controllers;
 
 import art.lenn.didemo.services.InjectionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -10,7 +11,9 @@ public class FieldInjectionController {
     // Field needs @autowired annotation
     // We don't need to specify a qualifier here because spring finds the correct bean by reflection
     // If multiple options are available, the name of the variable will be used to find a specific implementation
+    // If a @Primary service is available, this will be used instead of looking at the variable name. Test this by removing @Qualifier
     @Autowired
+    @Qualifier("injectionServiceImpl")
     public InjectionService injectionServiceImpl;
 
     public String testService(){
