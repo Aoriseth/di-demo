ad21467a1296164a7eca52d93b232d1408c34182
Lennart Cockx <Lennart.Cockx@faros.be>
2018-12-17 11:09:08 +0100

    Added part to demonstrate spring bean lifecycle Added PostProcessor to allow use of beforeInit and afterInit

---
+++ src/main/java/art/lenn/didemo/LifeCycleDemo.java
@@ -0,0 +1,72 @@
+package art.lenn.didemo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class LifeCycleDemo implements InitializingBean, DisposableBean, BeanNameAware, BeanFactoryAware, ApplicationContextAware {
+
+    public LifeCycleDemo() {
+        System.out.print(">> LifeCycleDemo - ");
+        System.out.println("Constructor");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.print(">> LifeCycleDemo - ");
+        System.out.println("setBeanFactory:"+beanFactory.toString());
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.print(">> LifeCycleDemo - ");
+        System.out.println("setBeanName:"+s);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.print(">> LifeCycleDemo - ");
+        System.out.println("Destroyed");
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        System.out.print(">> LifeCycleDemo - ");
+        System.out.println("afterPropertiesSet");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.print(">> LifeCycleDemo - ");
+        System.out.println("setApplicationContext:"+applicationContext.toString());
+    }
+
+    @PostConstruct
+    public void postCons(){
+        System.out.print(">> LifeCycleDemo - ");
+        System.out.println("PostConstruct");
+    }
+
+    @PreDestroy
+    public void preDest(){
+        System.out.print(">> LifeCycleDemo - ");
+        System.out.println("PreDestroy");
+    }
+
+    void beforeInit(){
+        System.out.print(">> LifeCycleDemo - ");
+        System.out.println("beforeInit");
+    }
+
+    void afterInit(){
+        System.out.print(">> LifeCycleDemo - ");
+        System.out.println("afterInit");
+    }
+
+}
